3869d933a3f2f2ed0d4aab935d06e53e03de09c0
VuLeHong <23560046@gm.uit.edu.vn>
2024-06-20 14:35:16 +0700

    adjust css

--- Front_end/src/components/Navbar/Navbar.jsx
+++ Front_end/src/components/Navbar/Navbar.jsx
@@ -1,40 +1,24 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import { CiUser } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
 import { Link } from 'react-router-dom'
-
-
+import { IoMdSearch } from "react-icons/io";
+import './Navbar.css'
 function NavScrollExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/shop">Shop</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/faq">FAQ</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          <Link to = '/login'><CiUser /></Link>
-          <CiShoppingCart />
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="navbar">
+      <div className="navbar-logo">VBCandle</div>
+      <div className="navbar-menu">
+        <a href="" className="navbar-link">Home</a>
+        <a href="" className="navbar-link">Shop</a>
+        <a href="" className="navbar-link">About</a>
+        <a href="" className="navbar-link">FAQ</a>
+      </div>
+      <div className="navbar-icon">
+        <IoMdSearch />
+        <Link to = '/login'><CiUser /></Link>
+        <CiShoppingCart />
+      </div>
+    </div>
   );
 }
 
