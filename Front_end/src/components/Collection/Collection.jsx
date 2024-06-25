96bc435d13033b55268bd9458aad1914eb808d20
unknown <binhlg.ti.2023@gmail.com>
2024-06-25 18:01:56 +0700

    front-end

--- Front_end/src/components/Collection/Collection.jsx
+++ Front_end/src/components/Collection/Collection.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import product1 from './product1.jpg'
+import '../Collection/Collection.css'
 
 const Collection = () => {
   return (
     <div className='collection'>
+      <div className="collection-title">
         <h1>FEATURED COLLECTION</h1>
+      </div>
         <div className="collection-map">
+          <div className='collection-map-list'>
             <img src={product1} />
-            <br />
             <p>On Vacation</p>
-            <br />
             <p>$35.00</p>
-            <br />
+          </div>
+          <div className='collection-map-list'>
+            <img src={product1} />
+            <p>On Vacation</p>
+            <p>$35.00</p>
+          </div>
+          <div className='collection-map-list'>
+            <img src={product1} />
+            <p>On Vacation</p>
+            <p>$35.00</p>
+          </div>
+          <div className='collection-map-list'>
+            <img src={product1} />
+            <p>On Vacation</p>
+            <p>$35.00</p>
+          </div>
+          <div className='collection-map-list'>
+            <img src={product1} />
+            <p>On Vacation</p>
+            <p>$35.00</p>
+          </div>
+          <div className='collection-map-list'>
+            <img src={product1} />
+            <p>On Vacation</p>
+            <p>$35.00</p>
+          </div>
         </div>
     </div>
   )
