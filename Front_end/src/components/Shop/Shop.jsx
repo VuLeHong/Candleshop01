7519534c4d31471a431304d92b6cc8278e2a314d
unknown <binhlg.ti.2023@gmail.com>
2024-06-27 07:57:33 +0700

    .

--- Front_end/src/components/Shop/Shop.jsx
+++ Front_end/src/components/Shop/Shop.jsx
@@ -7,31 +7,67 @@ const Shop = () => {
   return (
     <div className='shop'>
         <div className="shop-top">
-            <p>Products</p>
+            <h1>Products</h1>
         </div>
         <div className="shop-middle">
-            <p>SORT BY</p>
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic" className='drop-down-sort'>
-                    Dropdown Button
-                </Dropdown.Toggle>  
-                <Dropdown.Menu className='drop-down-sort'>
-                    <Dropdown.Item href="#/action-1">Featured</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Best selling</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Alphabetically, A-Z</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Alphabetically, Z-A</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Price, low to high</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Price, high to low</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Date, old to new</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Date, new to old</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-            <p>5 products</p>
+            <div className="shop-middle-left">
+                <p>SORT BY</p>
+                <Dropdown>
+                    <Dropdown.Toggle variant="antiquewhite" id="dropdown-basic">
+                        Alphabetically, A-Z
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Alphabetically, Z-A</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Price, low to high</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Price, high to low</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+            <div className="shop-middle-right">
+                <p>5 products</p>
+            </div>
         </div>
         <div className="shop-bottom">
-            <img src={product1} />
-            <p>Baked Goods | MICANDLE</p>
-            <p>$35.00</p>
+            <div className="shop-bottom-list">
+                <img src={product1} />
+                <p>Baked Goods | MICANDLE</p>
+                <p>$35.00</p>
+            </div>
+            <div className="shop-bottom-list">
+                <img src={product1} />
+                <p>Baked Goods | MICANDLE</p>
+                <p>$35.00</p>
+            </div>
+            <div className="shop-bottom-list">
+                <img src={product1} />
+                <p>Baked Goods | MICANDLE</p>
+                <p>$35.00</p>
+            </div>
+            <div className="shop-bottom-list">
+                <img src={product1} />
+                <p>Baked Goods | MICANDLE</p>
+                <p>$35.00</p>
+            </div>
+            <div className="shop-bottom-list">
+                <img src={product1} />
+                <p>Baked Goods | MICANDLE</p>
+                <p>$35.00</p>
+            </div>
+            <div className="shop-bottom-list">
+                <img src={product1} />
+                <p>Baked Goods | MICANDLE</p>
+                <p>$35.00</p>
+            </div>
+            <div className="shop-bottom-list">
+                <img src={product1} />
+                <p>Baked Goods | MICANDLE</p>
+                <p>$35.00</p>
+            </div>
+            <div className="shop-bottom-list">
+                <img src={product1} />
+                <p>Baked Goods | MICANDLE</p>
+                <p>$35.00</p>
+            </div>
         </div>
     </div>
   )
