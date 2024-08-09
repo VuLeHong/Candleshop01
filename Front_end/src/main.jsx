86e6bda29124aec1d7b49b3cf04b5198959e0899
VuLeHong <Catcanroll104@gmail.com>
2024-08-09 09:44:02 +0700

    final

--- Front_end/src/main.jsx
+++ Front_end/src/main.jsx
@@ -2,24 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './components/Home/Home.jsx'
-import Shop from './components/Shop/Shop.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/shop",
-    element: <Shop />
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router}/>
+    <App />
+
   </React.StrictMode>,
 )
